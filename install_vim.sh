24bbc0be9138e5a588a2fbebd051aebb8f1eb3be
Panos Georgiadis <pgeorgiadis@suse.de>
2016-02-08 11:32:33 +0100

    Fix gnome colors if you have more than one profile

--- install_vim.sh
+++ install_vim.sh
@@ -15,24 +15,35 @@ cd $HOME
 #########
 # Gnome #
 #########
-# take your profile
-profile=$(dconf list /org/gnome/terminal/legacy/profiles:/)
 
-# Remove '/' from the end of the profile variable
-profile=$(echo "${profile::-1}")
+number_of_profiles=$(dconf list /org/gnome/terminal/legacy/profiles:/ | wc -l)
 
-# Use theme colors
-dconf write /org/gnome/terminal/legacy/profiles:/$profile/use-theme-colors "false"
+if [ "$number_of_profiles" -gt 1 ]; then
+  echo "You have more than 1 profiles and I am too lazy to configure them"
+  echo
+  echo "Go to:"
+  echo "Edit > Colors"
+  echo "Text and Background Color -->  Built-in schemes: Solarized dark"
+  echo "Palette --> Built-in schemes: Solarized"
+else
+  # take your profile
+  profile=$(dconf list /org/gnome/terminal/legacy/profiles:/)
 
-# Background color
-dconf write /org/gnome/terminal/legacy/profiles:/$profile/background-color "'rgb(0,43,54)'"
+  # Remove '/' from the end of the profile variable
+  profile=$(echo "${profile::-1}")
 
-# Foreground color
-dconf write /org/gnome/terminal/legacy/profiles:/$profile/foreground-color "'rgb(131,148,150)'"
+  # Use theme colors
+  dconf write /org/gnome/terminal/legacy/profiles:/$profile/use-theme-colors "false"
 
-# Use Transparent background
-dconf write /org/gnome/terminal/legacy/profiles:/$profile/use-transparent-background "false"
+  # Background color
+  dconf write /org/gnome/terminal/legacy/profiles:/$profile/background-color "'rgb(0,43,54)'"
 
-# Solarized Palete
-dconf write /org/gnome/terminal/legacy/profiles:/$profile/palette "['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
+  # Foreground color
+  dconf write /org/gnome/terminal/legacy/profiles:/$profile/foreground-color "'rgb(131,148,150)'"
 
+  # Use Transparent background
+  dconf write /org/gnome/terminal/legacy/profiles:/$profile/use-transparent-background "false"
+
+  # Solarized Palete
+  dconf write /org/gnome/terminal/legacy/profiles:/$profile/palette "['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
+fi
